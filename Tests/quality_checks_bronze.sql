6ab126b611d87be37e8ef2fc179a3a70b1df8b6f
Sumit Kumar Soni <Sumitsoni951123@gmail.com>
2025-05-29 16:04:27 +0530

    Update quality_checks_bronze.sql

--- Tests/quality_checks_bronze.sql
+++ Tests/quality_checks_bronze.sql
@@ -1,3 +1,22 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Bronze Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
 -- Check For NULL or Duplicates in Primary Key
 -- Expectation: No Result
 
